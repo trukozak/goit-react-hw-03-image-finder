b403620a0418d3ec94c1561e88f5829f4d837983
trukozak <aleksandr.nepijvoda@gmail.com>
2021-07-27 22:13:44 +0300

    add_project

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import Section from './Section/Section';
+
+class App extends Component {
+  state = {
+  };
+
+  render() {
+    const {  } = this.state;
+    return (
+      <>
+        <Section title="">
+        </Section>
+      </>
+    );
+  }
+}
+
+export default App;
